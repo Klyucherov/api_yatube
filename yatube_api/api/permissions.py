21457a250d00d6846cc51760017e3db40b9b5155
Klyucherov Evgeniy <Klyucherov.e@yandex.ru>
2022-08-01 14:51:31 +0300

    V2

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class OnlyAuthorChangeContent(BasePermission):
+class OnlyAuthorCanChangeContent(BasePermission):
     message = 'Только автор может изменить контент.'
 
     def has_object_permission(self, request, view, obj):
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,17 +1,16 @@
-from api.permissions import OnlyAuthorChangeContent
-from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
-
-from posts.models import Group, Post
-
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+
+from api.permissions import OnlyAuthorCanChangeContent
+from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
+from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [OnlyAuthorChangeContent, IsAuthenticated]
+    permission_classes = [OnlyAuthorCanChangeContent, IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -24,15 +23,14 @@ class GroupReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [OnlyAuthorChangeContent, IsAuthenticated]
+    permission_classes = [OnlyAuthorCanChangeContent, IsAuthenticated]
 
     def post_obj(self):
         queryset = get_object_or_404(Post, id=self.kwargs.get('post_id'))
         return queryset
 
     def get_queryset(self):
-        queryset = self.post_obj().comments.all()
-        return queryset
+        return self.post_obj().comments.all()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, post=self.post_obj())
