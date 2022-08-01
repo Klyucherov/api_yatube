aa4b49a7c793fe0a3f6c091ffa742efa39680136
Klyucherov Evgeniy <Klyucherov.e@yandex.ru>
2022-08-01 11:04:47 +0300

    v1.1

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,7 +5,6 @@ class OnlyAuthorChangeContent(BasePermission):
     message = 'Только автор может изменить контент.'
 
     def has_object_permission(self, request, view, obj):
-        permission = (
-                request.user == obj.author or request.method in SAFE_METHODS
-        )
+        permission = \
+            request.user == obj.author or request.method in SAFE_METHODS
         return permission
